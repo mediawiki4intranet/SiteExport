6cbd94dd7c985638ce6f5bbe4300c03aa9487739
Vitaliy Filippov <vitalif@yourcmc.ru>
2009-12-14 13:07:40 +0000

    SiteExport...

--- SiteExport.php
+++ SiteExport.php
@@ -51,9 +51,9 @@ function seGtsrHandleNews($article, $html)
 function seGtsrFixLinks($article, $html)
 {
     $html = preg_replace('!/wiki/index.php/GT-Service:_([^<>\'"#]*)!is', '/\1.htm', $html);
-    $html = preg_replace('#/wiki/index.php/(Image|Изображение|%D0%98%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5):([^<>\'"]*)#is', 'http://www.yourcmc.ru\0', $html);
     $html = str_replace('title="GT-Service:_', 'title="', $html);
     $html = str_replace('/wiki/images/', 'http://www.yourcmc.ru/wiki/images/', $html);
+    $html = preg_replace_callback('#/wiki/index.php/(Image|File|Файл|%D0%A4%D0%B0%D0%B9%D0%BB|Изображение|%D0%98%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5):([^<>\'"]*)#is', 'sePopupCallback', $html);
     $html = preg_replace('#<p(?:\s+[^<>]+)?>((?:\s*<!--.*?-->)*\s*)</p\s*>#is', '\1', $html);
     return $html;
 }
@@ -68,8 +68,9 @@ function seEmotionFixLinks($article, $html)
         $seEmotionSet = '<!--#set var="title" value="' . $article->getTitle()->getSubpageText() . '"-->' . $seEmotionSet;
     $html = preg_replace('!/wiki/index.php/Emotion/([^<>\'"#]*)!is', '/\1.htm', $html);
     $html = str_replace('title="Emotion/', 'title="', $html);
-    $html = preg_replace('#/wiki/index.php/(Image|Изображение|%D0%98%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5):([^<>\'"]*)#is', 'http://www.yourcmc.ru\0', $html);
     $html = str_replace('/wiki/images/', 'http://www.yourcmc.ru/wiki/images/', $html);
+    //$html = preg_replace('#/wiki/index.php/(Image|File|Файл|%D0%A4%D0%B0%D0%B9%D0%BB|Изображение|%D0%98%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5):([^<>\'"]*)#is', 'http://yourcmc.ru\0', $html);
+    $html = preg_replace_callback('#/wiki/index.php/(Image|File|Файл|%D0%A4%D0%B0%D0%B9%D0%BB|Изображение|%D0%98%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5):([^<>\'"]*)#is', 'sePopupCallback', $html);
     $html = preg_replace('#<p(?:\s+[^<>]+)?>((?:\s*<!--.*?-->)*\s*)</p\s*>#is', '\1', $html);
     return $html;
 }
@@ -87,6 +88,20 @@ function seEmotionCompose($article, $html)
 }
 
 /* ФУНКЦИИ РАСШИРЕНИЯ */
+
+function sePopupCallback($m)
+{
+    $title = urldecode($m[2]);
+    $img = Image::newFromName($title);
+    if ($img)
+    {
+        $w = $img->getWidth()+0;
+        $h = $img->getHeight()+0;
+        $url = $img->getFullURL();
+        return $url.'" rel="lightbox';
+    }
+    return '';
+}
 
 function mkpath($path, $mode = 0755)
 {
